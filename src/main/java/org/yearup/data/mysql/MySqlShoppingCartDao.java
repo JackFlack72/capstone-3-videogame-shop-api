0ad34edd425c5897719689e48b368defec38919e
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-18 10:08:10 -0600

    implement mysql shopping cart dao methods

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,4 +1,37 @@
 package org.yearup.data.mysql;
 
-public class MySqlShoppingCartDao {
+import org.springframework.stereotype.Component;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+    private ProductDao productDao;
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        return null;
+    }
+
+    @Override
+    public ShoppingCart addProductToCart(int userId, int productId) {
+        return null;
+    }
+
+    @Override
+    public void updateProductQuantity(int userId, int productId, int quantity) {
+
+    }
+
+    @Override
+    public void clearCart(int userId) {
+
+    }
+
+    public
+
+
+
+
 }
